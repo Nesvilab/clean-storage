dda912d1deec6c4d3e6e8d095ef5d840d6fa91f7
Fengchao <fcyucn@gmail.com>
2023-01-24 14:51:10 -0500

    Change `/` to `/storage/`

--- clean_storage.sh
+++ clean_storage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-find / -name "*.pepindex" -type f -delete 2> /dev/null
-find / -name "*.fragtmp" -type f -delete 2> /dev/null
-find / -name "*.mzBIN_calibrated" -type f -delete 2> /dev/null
-find / -name ".meta/*.bin" -type f -delete 2> /dev/null
-find / -name "*.serFS" -type f -delete 2> /dev/null
-find / -name "*.DIAWindowsFS" -type f -delete 2> /dev/null
-find / -name "*.RTidxFS" -type f -delete 2> /dev/null
-find / -name "*.ScanClusterMapping_Q1" -type f -delete 2> /dev/null
-find / -name "*.ScanClusterMapping_Q2" -type f -delete 2> /dev/null
-find / -name "*.ScanClusterMapping_Q3" -type f -delete 2> /dev/null
-find / -name "*.ScanidxFS" -type f -delete 2> /dev/null
-find / -name "*.ScanPosFS" -type f -delete 2> /dev/null
-find / -name "*.ScanRTFS" -type f -delete 2> /dev/null
-find / -name "*.ser" -type f -delete 2> /dev/null
+find /storage/ -name "*.pepindex" -type f -delete 2> /dev/null
+find /storage/ -name "*.fragtmp" -type f -delete 2> /dev/null
+find /storage/ -name "*.mzBIN_calibrated" -type f -delete 2> /dev/null
+find /storage/ -name ".meta/*.bin" -type f -delete 2> /dev/null
+find /storage/ -name "*.serFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.DIAWindowsFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.RTidxFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanClusterMapping_Q1" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanClusterMapping_Q2" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanClusterMapping_Q3" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanidxFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanPosFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.ScanRTFS" -type f -delete 2> /dev/null
+find /storage/ -name "*.ser" -type f -delete 2> /dev/null
 
