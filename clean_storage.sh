bcf53934c563f240390afaefa4ae5c24d721c9d3
Fengchao <fcyucn@gmail.com>
2023-03-25 11:48:19 -0400

    Make the command run in parallel

--- clean_storage.sh
+++ clean_storage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-find /storage/ -name "*.pepindex" -type f -delete 2> /dev/null
-find /storage/ -name "*.fragtmp" -type f -delete 2> /dev/null
-find /storage/ -name "*.mzBIN_calibrated" -type f -delete 2> /dev/null
-find /storage/ -name ".meta/*.bin" -type f -delete 2> /dev/null
-find /storage/ -name "*.serFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.DIAWindowsFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.RTidxFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanClusterMapping_Q1" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanClusterMapping_Q2" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanClusterMapping_Q3" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanidxFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanPosFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.ScanRTFS" -type f -delete 2> /dev/null
-find /storage/ -name "*.ser" -type f -delete 2> /dev/null
+find /storage/ -name "*.pepindex" -type f -delete 2> /dev/null &
+find /storage/ -name "*.fragtmp" -type f -delete 2> /dev/null &
+find /storage/ -name "*.mzBIN_calibrated" -type f -delete 2> /dev/null &
+find /storage/ -name ".meta/*.bin" -type f -delete 2> /dev/null &
+find /storage/ -name "*.serFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.DIAWindowsFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.RTidxFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanClusterMapping_Q1" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanClusterMapping_Q2" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanClusterMapping_Q3" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanidxFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanPosFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ScanRTFS" -type f -delete 2> /dev/null &
+find /storage/ -name "*.ser" -type f -delete 2> /dev/null &
 
